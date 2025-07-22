286ed99cb573fbbbbf9e16b81e190056a3516674
svictor <victorshayo92@gmail.com>
2025-07-23 01:22:20 +0300

    feat(api): Add GraphQL query definitions

---
+++ src/queries.js
@@ -0,0 +1,112 @@
+export const query = `
+   {
+  user {
+    id
+    login
+    attrs
+    auditRatio
+   transactions(where: {_and: [{eventId: {_eq: 75}}]}, order_by: {createdAt: asc}) {
+      object{
+        name
+        attrs
+        type
+      }
+      amount
+      createdAt
+      eventId
+      path
+      type
+    }
+     audits(
+      order_by: {createdAt: desc}
+      where: {closedAt: {_is_null: true},group: {captain: {canAccessPlatform: {_eq: true}}}}
+    ) {
+      closedAt
+      group {
+        captainLogin
+        path
+        members {
+          userLogin
+        }
+      }
+      private {
+        code
+      }
+    }
+  
+    events(where: {eventId: {_eq: 75}}) {
+      level
+    }
+    progresses(where: {eventId: {_eq: 75}} order_by: {createdAt: desc}) {
+      grade
+      path
+      createdAt
+      updatedAt
+    }
+  }
+  event(where: {path: {_eq: "/kisumu/module"}}) {
+    startAt
+    endAt
+  }
+  goItems: object(
+    where: {_or: [{type: {_eq: "project"}, attrs: {_contains: {language: "Go"}}}, {type: {_eq: "piscine"}, name: {_ilike: "%Go%"}}]}
+    distinct_on: [name]
+  ) {
+    name
+    type
+  }
+  jsItems: object(
+    where: {_or: [{type: {_eq: "project"}, attrs: {_contains: {language: "JavaScript"}}}, {type: {_eq: "piscine"}, name: {_ilike: "%JS%"}}]}
+    distinct_on: [name]
+  ) {
+    name
+    type
+  }
+  rustItems: object(
+    where: {_or: [{type: {_eq: "project"}, attrs: {_contains: {language: "rust"}}}, {type: {_eq: "piscine"}, name: {_ilike: "%Rust%"}}]}
+    distinct_on: [name]
+  ) {
+    name
+    type
+  }
+  
+  skill_types: user {
+    transactions_aggregate(
+      distinct_on: [type]
+      where: {type: {_nin: ["xp", "level", "up", "down"]}}
+      order_by: [{type: asc}, {amount: desc}]
+    ) {
+      nodes {
+        type
+        amount
+      }
+    }
+  }
+
+}
+`;
+
+
+export async function fetchGraphQL(token, variables = {}) {
+  try {
+    const response = await fetch(
+      "https://learn.zone01kisumu.ke/api/graphql-engine/v1/graphql",
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          query,
+        }),
+      }
+    );
+    if (!response.ok) {
+      throw new Error("GraphQL Request Failed");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("GraphQL Error:", error);
+  }
+}
